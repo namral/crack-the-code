d1db2922029fc19cbf9c0e5ace9d0e2a252d96a4
Namrata Lele <nlele@groupon.com>
2016-04-09 12:40:27 -0700

    permutation and powerset

--- src/main/nl/crack/problems/PowerSet.java
+++ src/main/nl/crack/problems/PowerSet.java
@@ -2,31 +2,51 @@ package main.nl.crack.problems;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 /**
  * Created by Namrata Lele on 2/14/16.
  * Return all subsets of a set
+ * http://www.programcreek.com/2013/01/leetcode-subsets-java/
  */
 public class PowerSet {
 
     public static void main(String[] args) {
-        List<List<String>> powerSet = computePowerSet(Arrays.asList("a", "b", "c"));
+        int[] s = {1, 2, 3};
+        List<List<Integer>> powerSet = subsets(s);
         System.out.println(powerSet);
     }
 
-    private static List<List<String>> computePowerSet(List<String> input) {
-        List<List<String>> powerSet = new ArrayList<>();
-        for (int i = 0; i < input.size(); i++) {
-            List<String> set = new ArrayList<>();
-            for (int j = i; j < input.size(); j++) {
-                set.add(input.get(j));
+    public static List<List<Integer>> subsets(int[] set) {
+        if (set == null)
+            return null;
+
+        Arrays.sort(set);
+        List<List<Integer>> result =  new ArrayList<>();
+
+        for (int i = 0; i < set.length; i++) {
+            List<List<Integer>> temp = new ArrayList<>();
+
+            //get sets that are already in result
+            for (List<Integer> a : result) {
+                temp.add(new ArrayList<>(a));
             }
-            powerSet.add(set);
-            powerSet.add(Arrays.asList(input.get(i)));
+
+            //add set[i] to existing sets
+            for (List<Integer> a : temp) {
+                a.add(set[i]);
+            }
+
+            //add set[i] only as a set
+            List<Integer> single = new ArrayList<>();
+            single.add(set[i]);
+            temp.add(single);
+
+            result.addAll(temp);
         }
-        return powerSet;
+        //add empty set
+        result.add(new ArrayList<Integer>());
+
+        return result;
     }
 }
--- src/main/nl/crack/problems/StringPermutation.java
+++ src/main/nl/crack/problems/StringPermutation.java
@@ -6,22 +6,24 @@ package main.nl.crack.problems;
  */
 public class StringPermutation {
     public static void main(String[] args) throws Exception {
-        permutation("abcd");
-
+        System.out.println("perm1 :");
+        permutation1("abcd");
+        System.out.println("perm2 :");
+        permutation2("abc");
     }
 
-    public static void permutation(String str) {
-        permutation(str.toCharArray(), 0, str.length() - 1);
+    public static void permutation1(String str) {
+        permutation1(str.toCharArray(), 0, str.length() - 1);
     }
 
-    private static void permutation(char[] input, int l, int r) {
+    private static void permutation1(char[] input, int l, int r) {
         if (l == r) {
             System.out.println(input);
         } else {
             for (int i = l; i <= r; i++) {
                 swap(input, l,i);
-                permutation(input, l+1, r);
-                swap(input, l,i);
+                permutation1(input, l + 1, r);
+                swap(input, l,i); //restore back input
             }
 
         }
@@ -31,5 +33,18 @@ public class StringPermutation {
         char temp = input[i1];
         input[i1] = input[i2];
         input[i2] = temp;
+    }
+
+    public static void permutation2(String str) {
+        permutation2("", str);
+    }
+
+    private static void permutation2(String prefix, String str) {
+        System.out.println("prefix : " + prefix + " String : "  + str);
+        int n = str.length();
+        if (n == 0) System.out.println(prefix);
+        for (int i = 0; i < str.length(); i++) {
+            permutation2(prefix + str.charAt(i), str.substring(0, i) + str.substring(i+1, n));
+        }
     }
 }
